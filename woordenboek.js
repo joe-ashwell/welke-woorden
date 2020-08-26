42ce6c14dc042a7e6c5a5c5e4e9ddac5cd749394
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-08-26 12:40:13 +0200

    Update woordenboek.js

--- woordenboek.js
+++ woordenboek.js
@@ -46,6 +46,27 @@ const werkwoordenList = [
   },
   {
     // TT
+    iktt: 'moet',
+    jijtt: 'moet',
+    hijtt: 'moet',
+    wijtt: 'moeten',
+    // OVT
+    ikovt: 'moest',
+    jijovt: 'moest',
+    hijovt: 'moest',
+    wijovt: 'moesten',
+    // VTT
+    ikvtt: 'heb gemoeten',
+    jijvtt: 'hebt gemoeten',
+    hijvtt: 'heeft gemoeten',
+    wijvtt: 'hebben gemoeten',
+    // Engels
+    engels: 'must',
+    // Infinitief
+    infin: 'moeten'
+  },
+  {
+    // TT
     iktt: 'wil',
     jijtt: 'wilt',
     hijtt: 'wilt',
@@ -68,13 +89,13 @@ const werkwoordenList = [
   {
     // TT
     iktt: 'zal',
-    jijtt: 'zult',
-    hijtt: 'zult',
+    jijtt: 'zal',
+    hijtt: 'zal',
     wijtt: 'zullen',
     // OVT
     ikovt: 'zou',
-    jijovt: 'zoude',
-    hijovt: 'zoude',
+    jijovt: 'zou',
+    hijovt: 'zou',
     wijovt: 'zouden',
     // VTT
     ikvtt: '',
@@ -89,6 +110,787 @@ const werkwoordenList = [
 
   // IRREGULAR werkwoorden
 
+  //! TO CHECK BY WINYA
+
+  {
+    // TT
+    iktt: 'begin',
+    jijtt: 'begint',
+    hijtt: 'begint',
+    wijtt: 'beginnen',
+    // OVT
+    ikovt: 'begon',
+    jijovt: 'begon',
+    hijovt: 'begon',
+    wijovt: 'begonnen',
+    // VTT
+    ikvtt: 'ben begonnen',
+    jijvtt: 'bent begonnen',
+    hijvtt: 'is begonnen',
+    wijvtt: 'zijn begonnen',
+    // Engels
+    engels: 'to begin',
+    // Infinitief
+    infin: 'beginnen'
+  },
+  {
+    // TT
+    iktt: 'beweeg',
+    jijtt: 'beweegt',
+    hijtt: 'beweegt',
+    wijtt: 'bewegen',
+    // OVT
+    ikovt: 'bewoog',
+    jijovt: 'bewoog',
+    hijovt: 'bewoog',
+    wijovt: 'bewogen',
+    // VTT
+    ikvtt: 'ben/heb bewogen',
+    jijvtt: 'bent/hebt bewogen',
+    hijvtt: 'is/heeft bewogen',
+    wijvtt: 'zijn/hebben bewogen',
+    // Engels
+    engels: 'to move',
+    // Infinitief
+    infin: 'bewegen'
+  },
+  {
+    // TT
+    iktt: 'bekijk',
+    jijtt: 'bekijkt',
+    hijtt: 'bekijkt',
+    wijtt: 'bekijken',
+    // OVT
+    ikovt: 'bekeek',
+    jijovt: 'bekeek',
+    hijovt: 'bekeek',
+    wijovt: 'bekeken',
+    // VTT
+    ikvtt: 'heb bekeken',
+    jijvtt: 'hebt bekeken',
+    hijvtt: 'heeft bekeken',
+    wijvtt: 'hebben bekeken',
+    // Engels
+    engels: 'to watch',
+    // Infinitief
+    infin: 'bekijken'
+  },
+  {
+    // TT
+    iktt: 'bied',
+    jijtt: 'biedt',
+    hijtt: 'biedt',
+    wijtt: 'bieden',
+    // OVT
+    ikovt: 'bood',
+    jijovt: 'bood',
+    hijovt: 'bood',
+    wijovt: 'boden',
+    // VTT
+    ikvtt: 'heb geboden',
+    jijvtt: 'hebt geboden',
+    hijvtt: 'heeft geboden',
+    wijvtt: 'hebben geboden',
+    // Engels
+    engels: 'to offer',
+    // Infinitief
+    infin: 'bieden'
+  },
+  {
+    // TT
+    iktt: 'blijk',
+    jijtt: 'blijkt',
+    hijtt: 'blijkt',
+    wijtt: 'blijken',
+    // OVT
+    ikovt: 'bleek',
+    jijovt: 'bleek',
+    hijovt: 'bleek',
+    wijovt: 'bleken',
+    // VTT
+    ikvtt: 'ben gebleken',
+    jijvtt: 'bent gebleken',
+    hijvtt: 'is gebleken',
+    wijvtt: 'zijn gebleken',
+    // Engels
+    engels: 'turn out',
+    // Infinitief
+    infin: 'blijken'
+  },
+  {
+    // TT
+    iktt: 'blijf',
+    jijtt: 'blijft',
+    hijtt: 'blijft',
+    wijtt: 'blijven',
+    // OVT
+    ikovt: 'bleef',
+    jijovt: 'bleef',
+    hijovt: 'bleef',
+    wijovt: 'bleven',
+    // VTT
+    ikvtt: 'ben gebleven',
+    jijvtt: 'bent gebleven',
+    hijvtt: 'is gebleven',
+    wijvtt: 'zijn gebleven',
+    // Engels
+    engels: 'to stay',
+    // Infinitief
+    infin: 'blijven'
+  },
+  {
+    // TT
+    iktt: 'breek',
+    jijtt: 'breekt',
+    hijtt: 'breekt',
+    wijtt: 'breken',
+    // OVT
+    ikovt: 'brak',
+    jijovt: 'brak',
+    hijovt: 'brak',
+    wijovt: 'braken',
+    // VTT
+    ikvtt: 'ben/heb gebroken',
+    jijvtt: 'bent/hebt gebroken',
+    hijvtt: 'is/heeft gebroken',
+    wijvtt: 'zijn/hebben gebroken',
+    // Engels
+    engels: 'to break',
+    // Infinitief
+    infin: 'breken'
+  },
+  {
+    // TT
+    iktt: 'breng',
+    jijtt: 'brengt',
+    hijtt: 'brengt',
+    wijtt: 'brengen',
+    // OVT
+    ikovt: 'bracht',
+    jijovt: 'bracht',
+    hijovt: 'bracht',
+    wijovt: 'brachten',
+    // VTT
+    ikvtt: 'ben/heb gebracht',
+    jijvtt: 'bent/hebt gebracht',
+    hijvtt: 'is/heeft gebracht',
+    wijvtt: 'zijn/hebben gebracht',
+    // Engels
+    engels: 'to bring',
+    // Infinitief
+    infin: 'brengen'
+  },
+  {
+    // TT
+    iktt: 'denk',
+    jijtt: 'denkt',
+    hijtt: 'denkt',
+    wijtt: 'denken',
+    // OVT
+    ikovt: 'dacht',
+    jijovt: 'dacht',
+    hijovt: 'dacht',
+    wijovt: 'dachten',
+    // VTT
+    ikvtt: 'heb gedacht',
+    jijvtt: 'hebt gedacht',
+    hijvtt: 'heeft gedacht',
+    wijvtt: 'hebben gedacht',
+    // Engels
+    engels: 'to think',
+    // Infinitief
+    infin: 'denken'
+  },
+  {
+    // TT
+    iktt: 'doe',
+    jijtt: 'doet',
+    hijtt: 'doet',
+    wijtt: 'doen',
+    // OVT
+    ikovt: 'deed',
+    jijovt: 'deed',
+    hijovt: 'deed',
+    wijovt: 'deden',
+    // VTT
+    ikvtt: 'heb gedaan',
+    jijvtt: 'hebt gedaan',
+    hijvtt: 'heeft gedaan',
+    wijvtt: 'hebben gedaan',
+    // Engels
+    engels: 'to do',
+    // Infinitief
+    infin: 'doen'
+  },
+  {
+    // TT
+    iktt: 'draag',
+    jijtt: 'draagt',
+    hijtt: 'draagt',
+    wijtt: 'dragen',
+    // OVT
+    ikovt: 'droeg',
+    jijovt: 'droeg',
+    hijovt: 'droeg',
+    wijovt: 'droegen',
+    // VTT
+    ikvtt: 'heb gedragen',
+    jijvtt: 'hebt gedragen',
+    hijvtt: 'heeft gedragen',
+    wijvtt: 'hebben gedragen',
+    // Engels
+    engels: 'to wear',
+    // Infinitief
+    infin: 'dragen'
+  },
+  {
+    // TT
+    iktt: 'drink',
+    jijtt: 'drinkt',
+    hijtt: 'drinkt',
+    wijtt: 'drinken',
+    // OVT
+    ikovt: 'dronk',
+    jijovt: 'dronk',
+    hijovt: 'dronk',
+    wijovt: 'dronken',
+    // VTT
+    ikvtt: 'heb gedronken',
+    jijvtt: 'hebt gedronken',
+    hijvtt: 'heeft gedronken',
+    wijvtt: 'hebben gedronken',
+    // Engels
+    engels: 'to drink',
+    // Infinitief
+    infin: 'drinken'
+  },
+  {
+    // TT
+    iktt: 'eet',
+    jijtt: 'eet',
+    hijtt: 'eet',
+    wijtt: 'eten',
+    // OVT
+    ikovt: 'at',
+    jijovt: 'at',
+    hijovt: 'at',
+    wijovt: 'aten',
+    // VTT
+    ikvtt: 'heb gegeten',
+    jijvtt: 'hebt gegeten',
+    hijvtt: 'heeft gegeten',
+    wijvtt: 'hebben gegeten',
+    // Engels
+    engels: 'to eat',
+    // Infinitief
+    infin: 'eten'
+  },
+  {
+    // TT
+    iktt: 'geef',
+    jijtt: 'geeft',
+    hijtt: 'geeft',
+    wijtt: 'geven',
+    // OVT
+    ikovt: 'gaf',
+    jijovt: 'gaf',
+    hijovt: 'gaf',
+    wijovt: 'gaven',
+    // VTT
+    ikvtt: 'heb gegeven',
+    jijvtt: 'hebt gegeven',
+    hijvtt: 'heeft gegeven',
+    wijvtt: 'hebben gegeven',
+    // Engels
+    engels: 'to give',
+    // Infinitief
+    infin: 'geven'
+  },
+  {
+    // TT
+    iktt: 'grijp',
+    jijtt: 'grijpt',
+    hijtt: 'grijpt',
+    wijtt: 'grijpen',
+    // OVT
+    ikovt: 'greep',
+    jijovt: 'greep',
+    hijovt: 'greep',
+    wijovt: 'grepen',
+    // VTT
+    ikvtt: 'heb gegrepen',
+    jijvtt: 'hebt gegrepen',
+    hijvtt: 'heeft gegrepen',
+    wijvtt: 'hebben gegrepen',
+    // Engels
+    engels: 'to grab',
+    // Infinitief
+    infin: 'grijpen'
+  },
+  {
+    // TT
+    iktt: 'hang',
+    jijtt: 'hangt',
+    hijtt: 'hangt',
+    wijtt: 'hangen',
+    // OVT
+    ikovt: 'hing',
+    jijovt: 'hing',
+    hijovt: 'hing',
+    wijovt: 'hingen',
+    // VTT
+    ikvtt: 'heb gehangen',
+    jijvtt: 'hebt gehangen',
+    hijvtt: 'heeft gehangen',
+    wijvtt: 'hebben gehangen',
+    // Engels
+    engels: 'to hang',
+    // Infinitief
+    infin: 'hangen'
+  },
+  {
+    // TT
+    iktt: 'help',
+    jijtt: 'helpt',
+    hijtt: 'helpt',
+    wijtt: 'helpen',
+    // OVT
+    ikovt: 'hielp',
+    jijovt: 'hielp',
+    hijovt: 'hielp',
+    wijovt: 'hielpen',
+    // VTT
+    ikvtt: 'heb geholpen',
+    jijvtt: 'hebt geholpen',
+    hijvtt: 'heeft geholpen',
+    wijvtt: 'hebben geholpen',
+    // Engels
+    engels: 'to help',
+    // Infinitief
+    infin: 'helpen'
+  },
+  {
+    // TT
+    iktt: 'kies',
+    jijtt: 'kiest',
+    hijtt: 'kiest',
+    wijtt: 'kiezen',
+    // OVT
+    ikovt: 'koos',
+    jijovt: 'koos',
+    hijovt: 'koos',
+    wijovt: 'kozen',
+    // VTT
+    ikvtt: 'heb gekozen',
+    jijvtt: 'hebt gekozen',
+    hijvtt: 'heeft gekozen',
+    wijvtt: 'hebben gekozen',
+    // Engels
+    engels: 'to choose',
+    // Infinitief
+    infin: 'kiezen'
+  },
+  {
+    // TT
+    iktt: 'koop',
+    jijtt: 'koopt',
+    hijtt: 'koopt',
+    wijtt: 'kopen',
+    // OVT
+    ikovt: 'kocht',
+    jijovt: 'kocht',
+    hijovt: 'kocht',
+    wijovt: 'kochten',
+    // VTT
+    ikvtt: 'heb gekocht',
+    jijvtt: 'hebt gekocht',
+    hijvtt: 'heeft gekocht',
+    wijvtt: 'hebben gekocht',
+    // Engels
+    engels: 'to buy',
+    // Infinitief
+    infin: 'kopen'
+  },
+  {
+    // TT
+    iktt: 'krijg',
+    jijtt: 'krijgt',
+    hijtt: 'krijgt',
+    wijtt: 'krijgen',
+    // OVT
+    ikovt: 'kreeg',
+    jijovt: 'kreeg',
+    hijovt: 'kreeg',
+    wijovt: 'kregen',
+    // VTT
+    ikvtt: 'heb gekregen',
+    jijvtt: 'hebt gekregen',
+    hijvtt: 'heeft gekregen',
+    wijvtt: 'hebben gekregen',
+    // Engels
+    engels: 'to get',
+    // Infinitief
+    infin: 'krijgen'
+  },
+  {
+    // TT
+    iktt: 'laat',
+    jijtt: 'laat',
+    hijtt: 'laat',
+    wijtt: 'laten',
+    // OVT
+    ikovt: 'liet',
+    jijovt: 'liet',
+    hijovt: 'liet',
+    wijovt: 'lieten',
+    // VTT
+    ikvtt: 'heb gelaten',
+    jijvtt: 'hebt gelaten',
+    hijvtt: 'heeft gelaten',
+    wijvtt: 'hebben gelaten',
+    // Engels
+    engels: '*to let*',
+    // Infinitief
+    infin: 'laten'
+  },
+  {
+    // TT
+    iktt: 'lees',
+    jijtt: 'leest',
+    hijtt: 'leest',
+    wijtt: 'lezen',
+    // OVT
+    ikovt: 'las',
+    jijovt: 'las',
+    hijovt: 'las',
+    wijovt: 'lazen',
+    // VTT
+    ikvtt: 'heb gelezen',
+    jijvtt: 'hebt gelezen',
+    hijvtt: 'heeft gelezen',
+    wijvtt: 'hebben gelezen',
+    // Engels
+    engels: 'to read',
+    // Infinitief
+    infin: 'lezen'
+  },
+  {
+    // TT
+    iktt: 'lig',
+    jijtt: 'ligt',
+    hijtt: 'ligt',
+    wijtt: 'liggen',
+    // OVT
+    ikovt: 'lag',
+    jijovt: 'lag',
+    hijovt: 'lag',
+    wijovt: 'lagen',
+    // VTT
+    ikvtt: 'heb gelegen',
+    jijvtt: 'hebt gelegen',
+    hijvtt: 'heeft gelegen',
+    wijvtt: 'hebben gelegen',
+    // Engels
+    engels: 'to lay',
+    // Infinitief
+    infin: 'liggen'
+  },
+  {
+    // TT
+    iktt: 'meet',
+    jijtt: 'meet',
+    hijtt: 'meet',
+    wijtt: 'meten',
+    // OVT
+    ikovt: 'mat',
+    jijovt: 'mat',
+    hijovt: 'mat',
+    wijovt: 'maten',
+    // VTT
+    ikvtt: 'heb gemeten',
+    jijvtt: 'hebt gemeten',
+    hijvtt: 'heeft gemeten',
+    wijvtt: 'hebben gemeten',
+    // Engels
+    engels: 'to measure',
+    // Infinitief
+    infin: 'meten'
+  },
+  {
+    // TT
+    iktt: 'neem',
+    jijtt: 'neemt',
+    hijtt: 'neemt',
+    wijtt: 'nemen',
+    // OVT
+    ikovt: 'nam',
+    jijovt: 'nam',
+    hijovt: 'nam',
+    wijovt: 'namen',
+    // VTT
+    ikvtt: 'heb genomen',
+    jijvtt: 'hebt genomen',
+    hijvtt: 'heeft genomen',
+    wijvtt: 'hebben genomen',
+    // Engels
+    engels: 'to take',
+    // Infinitief
+    infin: 'nemen'
+  },
+  {
+    // TT
+    iktt: 'sla op',
+    jijtt: 'slaat op',
+    hijtt: 'slaat op',
+    wijtt: 'slaan op',
+    // OVT
+    ikovt: 'sloeg op',
+    jijovt: 'sloeg op',
+    hijovt: 'sloeg op',
+    wijovt: 'sloegen op',
+    // VTT
+    ikvtt: 'heb opgeslagen',
+    jijvtt: 'hebt opgeslagen',
+    hijvtt: 'heeft opgeslagen',
+    wijvtt: 'hebben opgeslagen',
+    // Engels
+    engels: 'to save/store',
+    // Infinitief
+    infin: 'opslaan'
+  },
+  {
+    // TT
+    iktt: 'rijd',
+    jijtt: 'rijdt',
+    hijtt: 'rijdt',
+    wijtt: 'rijden',
+    // OVT
+    ikovt: 'reed',
+    jijovt: 'reed',
+    hijovt: 'reed',
+    wijovt: 'reden',
+    // VTT
+    ikvtt: 'ben/heb gereden',
+    jijvtt: 'bent/hebt gereden',
+    hijvtt: 'is/heeft gereden',
+    wijvtt: 'zijn/hebben gereden',
+    // Engels
+    engels: 'to ride/drive',
+    // Infinitief
+    infin: 'rijden'
+  },
+  {
+    // TT
+    iktt: 'roep',
+    jijtt: 'roept',
+    hijtt: 'roept',
+    wijtt: 'roepen',
+    // OVT
+    ikovt: 'riep',
+    jijovt: 'riep',
+    hijovt: 'riep',
+    wijovt: 'riepen',
+    // VTT
+    ikvtt: 'heb geroepen',
+    jijvtt: 'hebt geroepen',
+    hijvtt: 'heeft geroepen',
+    wijvtt: 'hebben geroepen',
+    // Engels
+    engels: 'to call',
+    // Infinitief
+    infin: 'roepen'
+  },
+  {
+    // TT
+    iktt: 'ruik',
+    jijtt: 'ruikt',
+    hijtt: 'ruikt',
+    wijtt: 'ruiken',
+    // OVT
+    ikovt: 'rook',
+    jijovt: 'rook',
+    hijovt: 'rook',
+    wijovt: 'roken',
+    // VTT
+    ikvtt: 'heb geroken',
+    jijvtt: 'hebt geroken',
+    hijvtt: 'heeft geroken',
+    wijvtt: 'hebben geroken',
+    // Engels
+    engels: 'to smell',
+    // Infinitief
+    infin: 'ruiken'
+  },
+  {
+    // TT
+    iktt: 'schiet',
+    jijtt: 'schiet',
+    hijtt: 'schiet',
+    wijtt: 'schieten',
+    // OVT
+    ikovt: 'schoot',
+    jijovt: 'schoot',
+    hijovt: 'schoot',
+    wijovt: 'schoten',
+    // VTT
+    ikvtt: 'heb geschoten',
+    jijvtt: 'hebt geschoten',
+    hijvtt: 'heeft geschoten',
+    wijvtt: 'hebben geschoten',
+    // Engels
+    engels: 'to shoot',
+    // Infinitief
+    infin: 'schieten'
+  },
+  {
+    // TT
+    iktt: 'schrijf',
+    jijtt: 'schrijft',
+    hijtt: 'schrijft',
+    wijtt: 'schrijven',
+    // OVT
+    ikovt: 'schreef',
+    jijovt: 'schreef',
+    hijovt: 'schreef',
+    wijovt: 'schreven',
+    // VTT
+    ikvtt: 'heb geschreven',
+    jijvtt: 'hebt geschreven',
+    hijvtt: 'heeft geschreven',
+    wijvtt: 'hebben geschreven',
+    // Engels
+    engels: 'to write',
+    // Infinitief
+    infin: 'schrijven'
+  },
+  {
+    // TT
+    iktt: 'schuif',
+    jijtt: 'schuift',
+    hijtt: 'schuift',
+    wijtt: 'schuiven',
+    // OVT
+    ikovt: 'schoof',
+    jijovt: 'schoof',
+    hijovt: 'schoof',
+    wijovt: 'schoven',
+    // VTT
+    ikvtt: 'heb geschoven',
+    jijvtt: 'hebt geschoven',
+    hijvtt: 'heeft geschoven',
+    wijvtt: 'hebben geschoven',
+    // Engels
+    engels: 'to slide',
+    // Infinitief
+    infin: 'schuiven'
+  },
+  {
+    // TT
+    iktt: 'sla',
+    jijtt: 'slaat',
+    hijtt: 'slaat',
+    wijtt: 'slaan',
+    // OVT
+    ikovt: 'sloeg',
+    jijovt: 'sloeg',
+    hijovt: 'sloeg',
+    wijovt: 'sloegen',
+    // VTT
+    ikvtt: 'heb geslagen',
+    jijvtt: 'hebt geslagen',
+    hijvtt: 'heeft geslagen',
+    wijvtt: 'hebben geslagen',
+    // Engels
+    engels: 'to hit',
+    // Infinitief
+    infin: 'slaan'
+  },
+  {
+    // TT
+    iktt: 'slaap',
+    jijtt: 'slaapt',
+    hijtt: 'slaapt',
+    wijtt: 'slapen',
+    // OVT
+    ikovt: 'sliep',
+    jijovt: 'sliep',
+    hijovt: 'sliep',
+    wijovt: 'sliepen',
+    // VTT
+    ikvtt: 'heb geslapen',
+    jijvtt: 'hebt geslapen',
+    hijvtt: 'heeft geslapen',
+    wijvtt: 'hebben geslapen',
+    // Engels
+    engels: 'to sleep',
+    // Infinitief
+    infin: 'slapen'
+  },
+  {
+    // TT
+    iktt: 'sluit',
+    jijtt: 'sluit',
+    hijtt: 'sluit',
+    wijtt: 'sluiten',
+    // OVT
+    ikovt: 'sloot',
+    jijovt: 'sloot',
+    hijovt: 'sloot',
+    wijovt: 'sloten',
+    // VTT
+    ikvtt: 'ben/heb gesloten',
+    jijvtt: 'bent/hebt gesloten',
+    hijvtt: 'is/heeft gesloten',
+    wijvtt: 'zijn/hebben gesloten',
+    // Engels
+    engels: 'to close',
+    // Infinitief
+    infin: 'sluiten'
+  },
+  {
+    // TT
+    iktt: 'snijd',
+    jijtt: 'snijdt',
+    hijtt: 'snijdt',
+    wijtt: 'snijden',
+    // OVT
+    ikovt: 'sneed',
+    jijovt: 'sneed',
+    hijovt: 'sneed',
+    wijovt: 'sneden',
+    // VTT
+    ikvtt: 'heb gesneden',
+    jijvtt: 'hebt gesneden',
+    hijvtt: 'heeft gesneden',
+    wijvtt: 'hebben gesneden',
+    // Engels
+    engels: 'to cut',
+    // Infinitief
+    infin: 'snijden'
+  },
+  {
+    // TT
+    iktt: 'spreek',
+    jijtt: 'spreekt',
+    hijtt: 'spreekt',
+    wijtt: 'spreken',
+    // OVT
+    ikovt: 'sprak',
+    jijovt: 'sprak',
+    hijovt: 'sprak',
+    wijovt: 'spraken',
+    // VTT
+    ikvtt: 'heb gesproken',
+    jijvtt: 'hebt gesproken',
+    hijvtt: 'heeft gesproken',
+    wijvtt: 'hebben gesproken',
+    // Engels
+    engels: 'to speak',
+    // Infinitief
+    infin: 'spreken'
+  },
+
+  //! TO HERE
   {
     // TT
     iktt: 'loop',
